27924dc77c283fb54e5cebc43f54b7f447e6c586
Gergely Revay <gergely.revay@gmail.com>
2012-03-10 19:37:35 +0100

    Added 'folloe me' menu item for rss feeds.

--- functions.php
+++ functions.php
@@ -4,8 +4,11 @@ function get_menu() { ?>
         <div id="menu">
             <ul id="nav">
                 <?php wp_list_pages('sort_column=menu_order&depth=1&title_li=');?>
+            <li class="page_item">
+                <a class="rss" title="" href="http://localhost/wordpress/?feed=rss2">follow me</a>
+            </li>
             </ul>
-            <?php include (TEMPLATEPATH . '/searchform.php'); ?>
+        <?php include (TEMPLATEPATH . '/searchform.php');?>
         </div>
     <!-- / menu -->
 <?php
