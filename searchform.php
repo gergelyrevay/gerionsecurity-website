e82ab3798503ef2ec9749e81f382c99ec467eb76
Gergely Revay <gergely.revay@gmail.com>
2012-03-10 19:36:14 +0100

    Fixed search form to actually search.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 	<label class="hidden" for="s"><?php _e(''); ?></label>
-	<input type="text" value="<?php the_search_query(); ?>" name="search input" id="search_input" class="inputbox" />
-	<input type="submit" id="searchsubmit" value="GO" />
+	<input type="text" value="<?php the_search_query(); ?>" name="s" id="search_input" class="inputbox" />
+	<input type="submit" id="searchsubmit" value=">" />
 </form>
