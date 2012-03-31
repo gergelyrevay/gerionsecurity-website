399d8a07669e138c038a475171d4f59e7463b283
Gergely Revay <gergely.revay@gmail.com>
2012-04-01 00:24:33 +0200

    Relative link for follow me menu item.

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function get_menu() { ?>
             <ul id="nav">
                 <?php wp_list_pages('sort_column=menu_order&depth=1&title_li=');?>
             <li class="page_item">
-                <a class="rss" title="" href="http://localhost/wordpress/?feed=rss2">follow me</a>
+                <a id="follow-me-menu" title="" href="/wordpress/?feed=rss2">follow me</a>
             </li>
             </ul>
         <?php include (TEMPLATEPATH . '/searchform.php');?>
@@ -13,4 +13,17 @@ function get_menu() { ?>
     <!-- / menu -->
 <?php
 }
+
+if(!function_exists('_log')){
+  function _log( $message ) {
+    if( WP_DEBUG === true ){
+      if( is_array( $message ) || is_object( $message ) ){
+        error_log( print_r( $message, true ) );
+      } else {
+        error_log( $message );
+      }
+    }
+  }
+}
+
 ?>
