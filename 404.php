ed922adc2101a6780d09e3434267942c3d991286
Gergely Revay <gergely.revay@gmail.com>
2012-04-27 21:37:15 +0200

    Showed my endless humor on error page.

--- 404.php
+++ 404.php
@@ -1,10 +1,17 @@
 <?php get_header(); ?>
-<?php get_sidebar(); ?>
-
-	<div id="content">
-        <h2>...Sorry there is nothing like that here...</h2>
-    
+<div id="sidebar_and_content">
+<div id="sidebar">
+<div id="sidebar-double-wrapper">
+<?php get_menu(); ?>
+</div>
+</div>
+<div id="content">
+	<div class='shareinpost'>
+	<?php my_share_links(); ?>
 	</div>
+	<div class='title'>These aren't the droids you are looking for...</div>
+	<div class='title'>(Page not found)</div>
+</div>
 	<!--/content -->
 
 <?php get_footer(); ?>
