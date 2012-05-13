41907ea10c74729561108a7e27f0bad3b3d38c73
Gergely Revay <gergely.revay@gmail.com>
2012-05-13 09:38:27 +0000

    added class for in-post lists

---
+++ error-404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+	<div id="content">
+        <h2>...Sorry there is nothing like that here...</h2>
+    
+	</div>
+	<!--/content -->
+
+<?php get_footer(); ?>
